f94db52d1f4fff4fdb2507980ff52f5f7b36624a
Peter Terentev <petrfalkovsky@yandex.ru>
2022-12-03 22:39:30 +0300

    feat: 02-numbers-tasks all tasks done

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -222,8 +222,10 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const tryConvert = Number(value);
+  if (Number.isNaN(tryConvert)) return def;
+  return tryConvert;
 }
 
 module.exports = {
